0a65ee518451c14404f316407419e88ff9b9df1f
Antonio Vatafu <137089208+portofoliox@users.noreply.github.com>
2025-07-25 14:17:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Starting panel in background..."
 nohup node panel.js > /dev/null 2>&1 &
-echo "Panel started. Logs: logs/panel.log"
+echo "Panel started in the background."
